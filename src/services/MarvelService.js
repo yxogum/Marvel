e93cacb32f78b87279cc9c33a821f6e7f7bb86fa
Kuandikov Askar <askarkuandikov31.05@gmail.com>
2022-12-18 04:10:54 +0600

    added API

---
+++ src/services/MarvelService.js
@@ -0,0 +1,26 @@
+class MarvelService {
+  _apiBase = "https://gateway.marvel.com:443/v1/public/";
+  _apiKey = "apikey=ca852cbcdbcefa0b24983c740efbdf95";
+
+  getResourse = async (url) => {
+    let res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+  };
+
+  getAllCharacters = () => {
+    return this.getResourse(
+      `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
+    );
+  };
+
+  getCharacter = (id) => {
+    return this.getResourse(`${this._apiBase}characters/${id}?${this._apiKey}`);
+  };
+}
+
+export default MarvelService;
